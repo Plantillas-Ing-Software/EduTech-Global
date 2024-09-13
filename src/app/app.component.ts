445db9aa659c288a877c66d91eefa4cdc97e7d2c
XaerickSReborn <u202213358@upc.edu.pe>
2024-09-12 19:10:30 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'upcpre202402si729pc1sw56u202213358';
+}
