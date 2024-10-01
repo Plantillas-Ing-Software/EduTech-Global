5c653f73a3441804c43b5688ed96027d1a542701
XaerickSReborn <u202213358@upc.edu.pe>
2024-10-01 18:18:03 -0500

    feat(universities): implemented universities visualization.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {StatesListComponent} from "./universities/components/states-list/states-list.component";
+import {MatToolbar} from "@angular/material/toolbar";
+import {FooterContentComponent} from "./public/components/footer-content/footer-content.component";
+import {TranslateService} from "@ngx-translate/core";
+import {LanguageSwitcherComponent} from "./public/components/language-switcher/language-switcher.component";
+import {MatCardHeader} from "@angular/material/card";
+  import { MatCardSubtitle, MatCardTitle} from "@angular/material/card";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StatesListComponent, MatToolbar, FooterContentComponent, LanguageSwitcherComponent,MatCardSubtitle, MatCardHeader, MatCardTitle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'upcpre202402si729pc1sw56u202213358';
+  title = 'Science-Focused Universities';
+  constructor(translate: TranslateService) {
+    translate.setDefaultLang('es');
+    translate.use('en');
+  }
 }
